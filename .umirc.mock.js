59f8eece9e771d078355c86aa45ce29191d1aeab
LUKE <luke.bei.2015@gmail.com>
2018-05-30 16:25:36 +0800

    [ test] 添加mock

--- .umirc.mock.js
+++ .umirc.mock.js
@@ -1,3 +1,15 @@
+const fake_maillog = {
+    data:[
+        {
+            title: "邮件模块报警",
+            description: "邮件模块请求失败",
+        },
+    ]
+}
+
 
 export default {
+    '/api/maillog'(req,res) {
+        res.json(fake_maillog)
+    }
 };
